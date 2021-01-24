6e4c55850205fd3f7f7e4a3fe9f899c4a862fdc4
Ryth-cs <rts512@york.ac.uk>
2021-01-24 02:57:22 +0000

    Included TTT, PropHunt and Homicide

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ FROM cm2network/steamcmd:root
 ENV STEAMAPPID 4020
 ENV STEAMAPP garrysmod
 ENV STEAMAPPDIR "${HOMEDIR}/${STEAMAPP}-dedicated"
+ENV GAMETYPE "TTT"
 
 COPY entry.sh ${HOMEDIR}/entry.sh
 RUN set -x \
@@ -18,6 +19,13 @@ RUN set -x \
 		echo 'app_update '"${STEAMAPPID}"''; \
 		echo 'quit'; \
 	   } > "${HOMEDIR}/${STEAMAPP}_update.txt" \
+    && { \
+		echo '"mountcfg"'; \
+		echo '{'; \
+		echo '"cstrike"	"'${HOMEDIR}'/css/cstrike"'; \
+		echo '"tf"	"'${HOMEDIR}'/tf2/tf"'; \
+		echo '}'; \
+	   } > "${HOMEDIR}/mount.cfg" \
 	&& chmod +x "${HOMEDIR}/entry.sh" \
 	&& chown -R "${USER}:${USER}" "${HOMEDIR}/entry.sh" "${STEAMAPPDIR}" "${HOMEDIR}/${STEAMAPP}_update.txt" \
 	&& rm -rf /var/lib/apt/lists/* 
--- entry.sh
+++ entry.sh
@@ -1,16 +1,72 @@
 mkdir -p "${STEAMAPPDIR}" || true 
 
+
+if [[ ! -d "${HOMEDIR}/css" ]]
+then
+    echo "CSS and TF2 not installed, installing"
+
+    # CSS
+    bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
+                    +force_install_dir "${HOMEDIR}/css" \
+                    +app_update "232330" \
+                    +quit
+
+    # TF2
+    bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
+                    +force_install_dir "${HOMEDIR}/tf2" \
+                    +app_update "232250" \
+                    +quit
+fi
+
+# GMOD
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
+# Mount CSS and TF2
+cp "${HOMEDIR}/mount.cfg" "${STEAMAPPDIR}/garrysmod/cfg/"
+
 cd ${STEAMAPPDIR}
 
+case "$GAMETYPE" in
+    "TTT")
+        echo "Configuring TTT Server"
+        game_type="terrortown"
+        workshop_collection="2120021421"
+        default_map="ttt_minecraftcity_v4" ;;
+    "Prop")
+        echo "Configuring PropHunt Server"
+        game_type="prophunters"
+        workshop_collection="2120273717"
+        default_map="ph_lotparking" ;;
+    # Needs adding
+    "Murder")
+        echo "Configuring Murder Server"
+        game_type="murder"
+        workshop_collection="2120021421"
+        default_map="" ;;
+    "Homicide")
+        echo "Configuring Homicide Server"
+        game_type="homicide"
+        workshop_collection="2120176172"
+        default_map="ttt_Clue_se" ;;
+    *)
+        echo "No gametype was set so running sandbox"
+        game_type="sandbox"
+        workshop_collection=""
+        default_map="gm_flatgrass" ;;
+esac
+
+# Run GMOD
 bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
 			-steam_dir "${STEAMCMDDIR}" \
 			-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
 			-usercon \
 			-port "${SRCDS_PORT}" \
-			+map "da_rooftops" \
+            +gamemode "$game_type" \
+			+map "$default_map" \
+            +host_workshop_collection "$workshop_collection" \
 			+sv_setsteamaccount "${SRCDS_TOKEN}"
+
+
