1bf46a67af42cdd05e0ed412000e74cad1ad1170
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-10-27 23:09:10 +0100

    fix: add sv_lan 0

--- entry.sh
+++ entry.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 # Run GMOD, passing commands through
 ./srcds_run -game garrysmod -console -usercon \
     -port 27015 \
+    +sv_lan 0 \
     +gamemode "${GAMETYPE}" \
     +map "${DEFAULT_MAP}" \
     +host_workshop_collection "${WORKSHOP_COLLECTION}" \
